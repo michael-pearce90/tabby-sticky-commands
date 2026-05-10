567d1f4b39219ef79dab8a738cec763a2b2d48f5
HV-Michael <michael@hostingverified.com>
2026-05-10 02:15:44 +0100

    Add minimal sticky command header test build (#2)
    * Add minimal sticky command header test build
    
    * Remove malformed gitattributes

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+﻿import { NgModule } from '@angular/core'
+import TabbyCoreModule from 'tabby-core'
+import { TerminalDecorator } from 'tabby-terminal'
+import { StickyCommandHeaderDecorator } from './stickyCommandHeaderDecorator'
+
+@NgModule({
+  imports: [
+    TabbyCoreModule,
+  ],
+  providers: [
+    {
+      provide: TerminalDecorator,
+      useClass: StickyCommandHeaderDecorator,
+      multi: true,
+    },
+  ],
+})
+export default class StickyCommandHeaderModule { }
